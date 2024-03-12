4f1d0cac6132380cddf5de404b2b521843e4d84f
Lancelot Thoré <144225580+LancelotThore@users.noreply.github.com>
2024-03-12 11:58:23 +0100

    start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Root from './routes/root.jsx';
+import ErrorPage from './ui/ErrorPage/index.jsx';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  }
+]);
+
+
+const rootElement = document.querySelector('#root');
+
+if (rootElement) {
+  ReactDOM.createRoot(document.querySelector('#root')).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
+} else {
+  console.error('No root element found');
+}

062d2e3082a01090518a255f2b3e62858aac1743
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-03-25 15:45:51 +0100

    pas grand chose

--- src/components/Button/Type.jsx
+++ src/components/Button/Type.jsx
@@ -3,8 +3,8 @@ import { Dent } from "../../components/Button/Icons";
 export default function Type() {
     return (
         <>
-        <div className="rounded-lg bg-forground">
-            <div className="rounded-full w-5 bg-background">
+        <div className="rounded-lg bg-forground flex items-center justify-center">
+            <div className="flex rounded-full items-center justify-center w-14 bg-background">
             <Dent></Dent>
             </div>
             <h2 className="text-text">Les caries</h2>

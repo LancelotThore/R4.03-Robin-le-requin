1324f8202ca436db0d87e267d7eb11a2d870c5f7
NolanDuthil <144225548+NolanDuthil@users.noreply.github.com>
2024-03-29 01:20:26 +0100

    chrono

---
+++ src/ui/Chrono/Chrono.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Requin3ans, Dent, Candy, Couverts, Calendar, Chrono, Sante, DentsRemplie, Soda, Bonbon, PateTartine, Gateau, Cereale, Chips, Biscuit, Compte, Requin6ans, Requin9ans, Retour } from "../../components/Button/Icons";
+import Robin from '../../components/Button/Robin';
+import Timer from '../../components/Button/timer';
+
+export default function ChronoPage() {
+    return (
+        <div className=" pb-5 flex bg-background items-center justify-center flex-col gap-8">
+        <Retour className="absolute left-0 top-0 mt-7 ml-7" />
+        <Robin />
+        <Timer/>
+        </div>
+    )
+}
